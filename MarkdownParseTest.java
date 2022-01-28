e9b4171a0319c4a320b32cae7318632c20aefe61
azc001 <azc001@ucsd.edu>
2022-01-28 15:04:57 -0800

    h

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -12,19 +12,19 @@ public class MarkdownParseTest {
     }
     @Test
     public void testFile1() throws IOException{
-        String contents = Files.readString(Path.of("./test-file.md"));
+        String contents = Files.readString(Path.of("./test-file-1.md"));
         List<String> expect = List.of("https://something.com", "some-page.html");
         assertEquals(expect, MarkdownParse.getLinks(contents));
     }
     @Test
     public void testFile2() throws IOException{
-        String contents = Files.readString(Path.of("./test-file2.md"));
+        String contents = Files.readString(Path.of("./test-file-2.md"));
         List<String> expect = List.of("thisisalink.com", "thisisanotherlink.org");
         assertEquals(expect, MarkdownParse.getLinks(contents));
     }
     @Test
     public void testFile3() throws IOException{
-        String contents = Files.readString(Path.of("./test-file3.md"));
+        String contents = Files.readString(Path.of("./test-file-3.md"));
         List<String> expect = List.of("link[1].com", "link[].org");
         assertEquals(expect, MarkdownParse.getLinks(contents));
     }
